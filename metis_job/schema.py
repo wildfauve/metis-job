8ec53f79e2cf6fc6419a0c95453e926b4976b255
Col Perks <wild.fauve@gmail.com>
2024-04-30 07:30:46 +1200

    fix column

--- metis_job/schema.py
+++ metis_job/schema.py
@@ -93,7 +93,7 @@ class Struct:
                   nullable: bool = False,
                   validator: Callable = always_valid_validator,
                   cell_builder: Callable = default_cell_builder):
-        self.schema = su.build_timestamp_field(term, self.vocab, nullable=nullable)
+        self.fields.append(su.build_timestamp_field(term, self.vocab, nullable=nullable))
         return self.callback
 
 
